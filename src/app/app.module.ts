b6bdf9deafd63497ae584e7ff6cf7ffb85530d1e
Erik Lopez <erik@eriklopez.work>
2018-08-11 19:32:08 -0500

    Adds shop list edit, shop list ingredient to module declarations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,17 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { RecipesComponent } from './recipes/recipes.component';
+import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/shopping-list-edit.component';
+import { ShoppingListIngredientComponent } from './shopping-list/shopping-list-ingredient/shopping-list-ingredient.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     ShoppingListComponent,
-    RecipesComponent
+    RecipesComponent,
+    ShoppingListEditComponent,
+    ShoppingListIngredientComponent
   ],
   imports: [
     BrowserModule
