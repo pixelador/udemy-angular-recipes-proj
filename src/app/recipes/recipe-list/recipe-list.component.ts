f6be28c1656db4cc1fe964fc03f991e584d3edb5
Erik Lopez <erik@eriklopez.work>
2018-08-18 22:09:27 -0500

    Adds dummy data

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  // type derived from recipe.model.ts
+  recipes: Recipe[] = [
+    new Recipe(
+      'Test Recipe 1',
+      'This is only a test1.',
+      'https://www.maxpixel.net/static/photo/1x/Comisa-Healthy-Tomato-Recipe-2930786.jpg'),
+    new Recipe(
+      'Test Recipe 2',
+      'This is only a test1.',
+      'https://www.maxpixel.net/static/photo/1x/Comisa-Healthy-Tomato-Recipe-2930786.jpg'),
+    new Recipe(
+      'Test Recipe 3',
+      'This is only a test1.',
+      'https://www.maxpixel.net/static/photo/1x/Comisa-Healthy-Tomato-Recipe-2930786.jpg')
+  ];
 
   constructor() { }
 
